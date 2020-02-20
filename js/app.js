95e51d2517da13d8ba90ced2637e74321ff8a43e
kadir ince <kadirincedev@gmail.com>
2020-02-20 21:56:57 +0300

    Create new game codes

--- js/app.js
+++ js/app.js
@@ -16,10 +16,7 @@ Türkçe:
 */
 
 let scores, roundScore, activePlayer;
-
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 let diceDOM = document.querySelector(".dice");
 diceDOM.style.display = "none";
@@ -69,10 +66,42 @@ function nextPlayer() {
   diceDOM.style.display = "none";
 }
 
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector("#score-0").textContent = (0).toString();
+  document.querySelector("#score-1").textContent = (0).toString();
+  document.querySelector("#current-0").textContent = (0).toString();
+  document.querySelector("#current-1").textContent = (0).toString();
+  document.querySelector("#name-0").textContent = "Player 1";
+  document.querySelector("#name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+}
+
 let btn_stop = document.querySelector(".btn-stop");
 btn_stop.addEventListener("click", function() {
   scores[activePlayer] += roundScore;
   document.querySelector("#score-" + activePlayer).textContent =
     scores[activePlayer];
-  nextPlayer();
+
+  if (scores[activePlayer] >= 20) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+    diceDOM.style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    nextPlayer();
+  }
 });
+
+document.querySelector(".btn-new").addEventListener("click", init);
