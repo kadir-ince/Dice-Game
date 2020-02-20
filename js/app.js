be0a34ae814ea60a580c125c63e818d19c430432
kadir ince <kadirincedev@gmail.com>
2020-02-20 17:08:37 +0300

    Added Files
    Added first imgs, css, js files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/*
+GAME RULES:
+
+- The Dice game has 2 players
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Skip', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+Türkçe:
+- Oyun 2 kişi ile oynanır 
+- Her elde oyuncu istediği kadar zar atar ve global skoruna eklenir
+- Eğer ki 1 attığı zar 1 gelirse o eldeki tüm puanını kaybeder
+- Eğer 'Skip' derse o eldeki puanı global puanına eklenir
+- Kim ilk 100 global puana ulaşırsa oyunu kazanır
+*/
